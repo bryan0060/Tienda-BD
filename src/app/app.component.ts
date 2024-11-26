382be03bba7aee71fc7cb79e365b89672828cea2
bryan0060 <ojalaifood@gmail.com>
2024-11-26 12:56:27 -0500

    Añadida barra de navegación simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListadoProductosComponent } from "./listado-productos/listado-productos.component";
+import { NavegacionComponent } from "./navegacion/navegacion.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListadoProductosComponent],
+  imports: [RouterOutlet, ListadoProductosComponent, NavegacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
